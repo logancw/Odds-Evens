06f12b76456cde8a56b259b1f4357ec993ef2f2d
clokyy <clokyymc@gmail.com>
2023-11-30 20:12:47 -0600

    + project

---
+++ README.md
@@ -0,0 +1,8 @@
+## Lab Goal :   This lab was designed to teach you how to use an array. 
+
+ 
+
+ 
+
+## Lab Description :  
+ Instantiate an array and load it with values.  Write methods to determine how many of the values in the array are odd and how many are even.  The methods should return arrays that contain the odds and evens.
---
+++ src/App.java
@@ -0,0 +1,32 @@
+class App {
+    //                                   A31 Odds & Evens
+    //                                   Logan Weisgerber
+    //                                      11/15/2023
+  
+  
+  
+  
+  
+    public static void main(String[] args) {
+      int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21}; // Main array
+      int[] odd = new int[10]; // 10 spots in the array
+      int[] even = new int[11]; // 11 Spots in the array
+  
+      int evenIndex = 0;
+      int oddIndex = 0;
+  
+      // Loop Breakdown 
+  
+      for(int i=0; i < arr.length; i++){ // Loops through the array
+        if(arr[i] % 2 == 0){ // Checks if i's modulus of 2 == 0
+          System.out.println("Odd: " + i); // For some reason it prints odd numbers so we add this to our odd array
+          odd[oddIndex] = i;
+          oddIndex++;
+        }else{
+          System.out.println("Even: " + i); // Even array.
+          even[evenIndex] = i; 
+          evenIndex++;
+        }
+      }
+    }
+  }
